42f9a8b800a23be3f07d33a720beddff489c9c3c
GusRodrigues <omykronbr@gmail.com>
2020-01-18 03:15:52 -0500

    doing

--- assignments/assignment1/resources/js/main.js
+++ assignments/assignment1/resources/js/main.js
@@ -1,23 +1,89 @@
-/**
- * Change the visibility of all Questions
- */
-window.onload = function () {
-    var questionList = [1,2,3,4,5];
-    questionList.forEach(element => {
-        document.getElementById("question" + element).style.display = "none";
-    });
-};
+var score = 0;
+function loadGame() {
+    /*
+    <div id="q1" class="card">
+        Question 1<br>
+        <span id="questionBox1"/>
+    </div>
+    */
+    for (var i = 0; i < this.questions.length; i++) {
+        var next = (1 + i);
+        var box = document.createElement("div");
+        var para = document.createElement("p");
 
-/**
- * Switch the visibility of a question.
- * @param {string} questionId the Id
- */
-function changeVisibility(questionId) {
-    var questionCard = document.getElementById(questionId);
+        box.id = "q" + next; // qi
+        box.className = "card box";
+        para.innerHTML += "Question " + next;
+        box.appendChild(para);        
+        
+        var item = document.createElement("span");
+        item.id = "questionBox"+next;
+        item.className = "question";
+        item.innerHTML = this.questions[i].question;
 
-    if (questionCard.style.display === "none") {
-        questionCard.style.display = "block";
-    } else {
-        questionCard.style.display = "none";
+        para = document.createElement("p");
+        para.innerHTML += _buildAnswer(questions[i].answer).innerHTML;
+        item.appendChild(para);
+
+        box.appendChild(item);
+        document.getElementById("questionBox").appendChild(box);
     }
 }
+
+function _buildAnswer(answer) {
+    if (answer === "True" || answer === "False") {
+        // True of False Button
+        // check answer
+        var para = document.createElement("p");
+        var btnTrue = document.createElement("button");
+        btnTrue.className = "button shadow";
+        btnTrue.innerHTML += "True";
+        para.appendChild(btnTrue);
+        btnFalse = document.createElement("button");
+        btnFalse.className = "button shadow";
+        btnFalse.innerHTML += "False";
+        para.appendChild(btnFalse);
+        
+        return para;
+    } else {
+        // prompt anser
+        // check answer
+        return "Prompt"
+    }
+}
+
+/**
+ * Game questions
+ */
+var questions = [
+    {
+        "question": "What is the meaning of life the universe and everything?",
+        "answer": "42"
+    },
+    {
+        "question":"What's the name of the fairy in Peter Pan?",
+        "answer":"Thinkerbell"
+    },
+    {
+        "question":"New York's Statue of Liberty was a gift from which country?",
+        "answer":""
+    },
+    {
+        "question": "A leap year has 365 days?",
+        "answer": "False"
+    },
+    {
+        "question": "",
+        "answer": ""
+    }
+]
+
+
+/**
+ * LOAD AFTER ON LOAD
+ */
+window.onload = function() {
+    
+    loadGame(); 
+    document.getElementsByClassName("score")[0].innerHTML = score ;
+};
