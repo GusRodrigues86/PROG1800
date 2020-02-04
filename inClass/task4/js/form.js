0e114dc6788276b39744969303b9dd210eb0459e
GusRodrigues <omykronbr@gmail.com>
2020-02-04 12:05:32 -0500

    inClass task 4 download files

---
+++ inClass/task4/js/form.js
@@ -0,0 +1,14 @@
+/*
+    The function formSubmit() is called when the form "myform" is submitted.
+    It should run some validations and show the output.
+*/
+function formSubmit(){
+   
+
+    //Write your code here
+
+
+
+    // Return false will stop the form from submitting and keep it on the current page.
+    return false;
+}
