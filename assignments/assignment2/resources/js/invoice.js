88364dcc7ce40ee3b1cfe1623634fd249ee12f54
GusRodrigues <omykronbr@gmail.com>
2020-02-19 10:00:07 -0500

    warning and tax calculation

--- assignments/assignment2/resources/js/invoice.js
+++ assignments/assignment2/resources/js/invoice.js
@@ -7,20 +7,23 @@
 
 window.onload = function () {
     // only the forms that attach the 
-    if (sessionStorage['customerInfo'] == null) { 
+    if (sessionStorage['customerInfo'] == null) {
+        this.sessionStorage['invalid'] = true;
         window.location = '../index.html';
     }
     createInvoice();
 };
+
 let customerInfo = JSON.parse(sessionStorage['customerInfo']);
 let deliveryInfo = JSON.parse(sessionStorage['deliveryInfo']);
 let productInfo = JSON.parse(sessionStorage['products']);
 let shipment = JSON.parse(sessionStorage['shipment']);
-
+const productCost = { "p1": 10, "p2": 20, "p3": 30 };
+const shipmentCost = [40, 30, 20, 10];
 
 function createInvoice() {
     let customer = document.getElementById("customer");
-            
+
     let cName = document.createElement("div");
     cName.className = 'groupLine';
     let cNameKey = document.createElement("div");
@@ -31,7 +34,7 @@ function createInvoice() {
     cNameValue.textContent = customerInfo.Name;
     customer.appendChild(cNameKey);
     customer.appendChild(cNameValue);
-    
+
     let cEmail = document.createElement("div");
     cEmail.className = 'groupLine';
     let cEmailKey = document.createElement("div");
@@ -42,7 +45,7 @@ function createInvoice() {
     cEmailValue.textContent = customerInfo.Email;
     customer.appendChild(cEmailKey);
     customer.appendChild(cEmailValue);
-    
+
     let cPhone = document.createElement("div");
     cPhone.className = 'groupLine';
     let cPhoneKey = document.createElement("div");
@@ -52,7 +55,7 @@ function createInvoice() {
     cPhoneValue.className = 'value';
     cPhoneValue.textContent = customerInfo['Phone']
     customer.appendChild(cPhoneKey);
-    customer.appendChild(cPhoneValue);  
+    customer.appendChild(cPhoneValue);
 
 
     // Delivery information
@@ -71,12 +74,12 @@ function createInvoice() {
     address.textContent = deliveryInfo.Address;
     br = document.createElement('br');
     deliveryValue.appendChild(address);
-    
+
 
     let cityPair = document.createElement('p');
     cityPair.textContent = deliveryInfo.City + ", " + deliveryInfo.Province;
     deliveryValue.appendChild(cityPair);
-    
+
     let postal = document.createElement('p');
     postal.textContent = deliveryInfo.Postal;
     deliveryValue.appendChild(postal);
@@ -84,6 +87,60 @@ function createInvoice() {
     delivery.appendChild(wrapper);
 
     // Product information
+    let productBox = document.getElementById("products");
+    let productLine = document.createElement('div');
+    productLine.className = 'groupLine'
+    productLine.id = 'p1';
+    let p1Key = document.createElement('div');
+    p1Key.className = 'key'
+    p1Key.textContent = productInfo.p1 + " Real Bigfoot Air Freshner @ $ " + productCost.p1.toFixed(2);
+    let p1Value = document.createElement('div');
+    p1Value.className = 'value'
+    p1Value.textContent = "$ " + (parseFloat(productInfo.p1) * parseFloat(productCost.p1)).toFixed(2);
+    productLine.appendChild(p1Key);
+    productLine.appendChild(p1Value);
+    productBox.appendChild(productLine)
+    //p2
+    productLine = document.createElement('div');
+    productLine.className = 'groupLine'
+    productLine.id = 'p2';
+    let p2Key = document.createElement('div');
+    p2Key.className = 'key'
+    p2Key.textContent = productInfo.p2 + " Fake Mustache @ $ " + productCost.p2.toFixed(2);
+    let p2Value = document.createElement('div');
+    p2Value.className = 'value'
+    p2Value.textContent = "$ " + (parseFloat(productInfo.p2) * parseFloat(productCost.p2)).toFixed(2);
+    productLine.appendChild(p2Key);
+    productLine.appendChild(p2Value);
+    productBox.appendChild(productLine)
+    //p3
+    productLine = document.createElement('div');
+    productLine.className = 'groupLine'
+    productLine.id = 'p3';
+    let p3Key = document.createElement('div');
+    p3Key.className = 'key'
+    p3Key.textContent = productInfo.p3 + " Yodeling Pants @ $ " + productCost.p3.toFixed(2);
+    let p3Value = document.createElement('div');
+    p3Value.className = 'value'
+    p3Value.textContent = "$ " + (parseFloat(productInfo.p3) * parseFloat(productCost.p3)).toFixed(2);
+    productLine.appendChild(p3Key);
+    productLine.appendChild(p3Value);
+    productBox.appendChild(productLine)
+    // shipping charges
+    productLine = document.createElement('div');
+    productLine.className = 'groupLine'
+    productLine.id = 'shipCharges';
+    let shippingChargesKey = document.createElement('div');
+    shippingChargesKey.className = 'key'
+    shippingChargesKey.textContent = "Shipping charges for " + shipment.shipmentTime + " day(s) delivery:";
+    
+    let shippingChargesValue = document.createElement('div');
+    shippingChargesValue.className = 'value'
+    shippingChargesValue.textContent = "$ " + (shipmentCost[parseInt(shipment.shipmentTime)]).toFixed(2);
+    productLine.appendChild(shippingChargesKey);
+    productLine.appendChild(shippingChargesValue);
+    productBox.appendChild(productLine)
+
 
     // SubTotal and taxes
     let before = document.getElementById("subtotal");
@@ -95,7 +152,7 @@ function createInvoice() {
     subTotalKey.textContent = 'Subtotal: ';
     let subTotalValue = document.createElement('div');
     subTotalValue.className = 'value';
-    subTotalValue.textContent = 'test'; // sum all the products
+    subTotalValue.textContent = '$ ' + getSubtotal().toFixed(2); // sum all the products
     subTotal.appendChild(subTotalKey);
     subTotal.appendChild(subTotalValue);
     // calculate taxes
@@ -103,10 +160,10 @@ function createInvoice() {
     taxes.className = 'groupLine'
     let taxesKey = document.createElement('div');
     taxesKey.className = 'key';
-    taxesKey.textContent = 'Taxes @ '; // append % of tax value
+    taxesKey.textContent = 'Taxes @ ' + getTotalTaxesByProvince() + "%";
     let taxesValue = document.createElement('div');
     taxesValue.className = 'value';
-    taxesValue.textContent = '$ 123'; // calculate based on the subtotal
+    taxesValue.textContent = '$ ' + calculateTaxes().toFixed(2); // calculate based on the subtotal
     taxes.appendChild(taxesKey);
     taxes.appendChild(taxesValue);
     before.appendChild(subTotal);
@@ -120,9 +177,71 @@ function createInvoice() {
     totalKey.className = 'key';
     totalKey.textContent = 'Total: ';
     let totalValue = document.createElement('div');
-    totalValue.className= 'value';
-    totalValue.textContent = 'calc'; //calculate total
+    totalValue.className = 'value';
+    totalValue.textContent = '$ ' + calculateTotal().toFixed(2); //calculate total
     box.appendChild(totalKey);
     box.appendChild(totalValue);
     total.appendChild(box);
+}
+
+/**
+ * With the province code, return the tax %
+ * @returns {Number} the tax % (i.e. 10% = 10)
+ */
+function getTotalTaxesByProvince() {
+    let province = deliveryInfo.Province;
+    let taxes;
+    switch (province) {
+
+        case "BC": taxes = 12;
+            break;
+        case "MB": taxes = 12;
+            break;
+        case "NB": taxes = 15;
+            break;
+        case "NL": taxes = 15;
+            break;
+        case "NS": taxes = 15;
+            break;
+        case "ON": taxes = 13;
+            break;
+        case "PE": taxes = 15;
+            break;
+        case "QC": taxes = 14.975;
+            break;
+        case "SK": taxes = 11;
+            break
+        default: taxes = 5;
+    }
+    return taxes;
+}
+
+/**
+ * Calculate the tax value from the subtotal
+ * @returns {Number} the tax total
+ */
+function calculateTaxes() {
+    return getSubtotal() * (parseFloat(getTotalTaxesByProvince()) / 100.0);
+
+}
+
+/**
+ * Calculate the subtotal (sum of products + shippment)
+ * @returns {Number} the subtotal
+ */
+function getSubtotal() {
+    let p1Value = productCost.p1 * parseFloat(productInfo.p1);
+    let p2Value = productCost.p2 * parseFloat(productInfo.p2);
+    let p3Value = productCost.p3 * parseFloat(productInfo.p3);
+    let shipmentValue = shipmentCost[parseInt(shipment.shipmentTime)];
+
+    return p1Value + p2Value + p3Value + shipmentValue;
+}
+
+/**
+ * Sum the total cost (subtotal + taxes)
+ * @returns {Number} the total
+ */
+function calculateTotal() {
+    return getSubtotal() + calculateTaxes();
 }
---
+++ assignments/assignment2/resources/js/main.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    let tag = this.document.getElementById('warning');
+    if (this.sessionStorage['invalid']) {
+        this.sessionStorage.removeItem("invalid");
+        tag.style.display = 'block';
+        // remove on click
+        tag.addEventListener('click', function () { tag.style.display = 'none' });
+    } else {
+        this.sessionStorage.removeItem("invalid");
+        tag.style.display = 'none';
+    }
+};
